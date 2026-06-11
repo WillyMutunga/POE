ad04776a0d934e9f51a9297e546775542a92ff19
WillyMutunga <wsharks003@gmail.com>
2026-06-11 15:37:15 +0300

    Auto-create media symlink in production on startup

--- backend/academic/apps.py
+++ backend/academic/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class AcademicConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'academic'
+
+    def ready(self):
+        from django.conf import settings
+        target_media = '/home1/headwayc/poe.headwaycollege.ac.ke/media'
+        if os.path.exists('/home1/headwayc/poe.headwaycollege.ac.ke') and not os.path.lexists(target_media):
+            try:
+                os.symlink(settings.MEDIA_ROOT, target_media)
+            except Exception:
+                pass
